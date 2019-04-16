ff4e66422ba35c601806baf5bc2dc78bc81b4bf5
Lucas Natoli <lucas.natoli@gmail.com>
2019-04-16 17:23:55 -0300

    ConsultingListitem correction

--- src/components/ConsultingList.js
+++ src/components/ConsultingList.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 import ConsultingListItem from './ConsultingListItem'
-import ListGroupItem from 'react-bootstrap/ListGroupItem';
+
 const ConsultingList = ({consulting, onConsultingClick}) => (
   <ListGroup>
     {consulting.map(consultancy => 
-      <ListGroupItem
+      <ConsultingListItem
         key={consultancy.ID}
         {...consultancy}
         onClick={()=> onConsultingClick(consultancy.ID)}
