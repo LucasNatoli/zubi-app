189c3928e2908e0a50a889bacb2a4e7a73d12ae5
Lucas Natoli <lucas.natoli@gmail.com>
2019-05-02 19:04:19 -0300

    logo visibility toogler on sider collapse

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -9,62 +9,69 @@ import './DashBoard.css';
 import { Route } from 'react-router-dom';
 
 class DashBoard extends React.Component {
-  
-    render() {
-        const {
-            Header, Content, Footer, Sider,
-          } = Layout;
+  constructor(props) {
+    super(props)
+    this.state = {
+      collapsed : false
+    }
+  }
+  render() {
 
-        return (
-          <Layout className="dashboard">
-            <Sider
-              breakpoint="lg"
-              collapsedWidth="0"
-              width="200"
-              onBreakpoint={(broken) => { console.log(broken); }}
-              onCollapse={(collapsed, type) => { console.log(collapsed ? "Colapsado" : "expandido"); }}
-            >
-              <div className="logo">
-                  <img src={Iso} alt="Zubi-Logo" className="logo-iso"/>
-                  <img src={Logo} alt="Zubi-Logo" className="logo-text"/>
-              </div>
-              <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-                <Menu.Item key="1">
-                  <Icon type="user" />
-                  <span className="nav-text">DashBoard</span>
-                </Menu.Item>
-                <Menu.Item key="2">
-                  <Icon type="video-camera" />
-                  <span className="nav-text">Consultorias</span>
-                </Menu.Item>
-                <Menu.Item key="3">
-                  <Icon type="upload" />
-                  <span className="nav-text">Capacitaciones</span>
-                </Menu.Item>
-                <Menu.Item key="4">
-                  <Icon type="user" />
-                  <span className="nav-text">Agenda</span>
-                </Menu.Item>
-                <Menu.Item key="5">
-                  <Icon type="user" />
-                  <span className="nav-text">Cerrar Session</span>
-                </Menu.Item>                
-              </Menu>
-            </Sider>
-            <Layout>
-              <Header className="header">
-                <div className="logo">
-                    <img src={IsoBlue} alt="Zubi-Logo" className="logo-iso"/>
-                </div>
-                <UserDetails/>
-              </Header>
-              <Content style={{ margin: '24px 16px 0' }}>
-                <div className="content" style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                  content
-                </div>
-              </Content>
-              <Footer style={{ textAlign: 'center' }}>
-                Footer
+    const {
+      Header, Content, Footer, Sider,
+    } = Layout;
+
+    return (
+      <Layout className="dashboard">
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          width="200"
+          onBreakpoint={(broken) => { console.log(broken); }}
+          onCollapse={(collapsed) => { this.setState({collapsed: collapsed}); }}
+        >
+          <div className="logo">
+            <img src={Iso} alt="Zubi-Logo" className="logo-iso" />
+            <img src={Logo} alt="Zubi-Logo" className="logo-text" />
+          </div>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span className="nav-text">DashBoard</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span className="nav-text">Consultorias</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span className="nav-text">Capacitaciones</span>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Icon type="user" />
+              <span className="nav-text">Agenda</span>
+            </Menu.Item>
+            <Menu.Item key="5">
+              <Icon type="user" />
+              <span className="nav-text">Cerrar Session</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header className="header">
+             
+            <div className={this.state.collapsed ? "logo" : "logo hidden"}>
+              <img src={IsoBlue} alt="Zubi-Logo" className="logo-iso" />
+            </div>
+            <UserDetails />
+          </Header>
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div className="content" style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+              <Route path="mis-consultorias" component={Consulting}/>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Footer
               </Footer>
         </Layout>
       </Layout>
