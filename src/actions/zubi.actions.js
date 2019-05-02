8fa437de4e4af79949ae2a3fa168b395ae8c179d
Lucas Natoli <lucas.natoli@gmail.com>
2019-05-02 13:06:51 -0300

    adding filtering and capacitaciones endpoint

--- src/actions/zubi.actions.js
+++ src/actions/zubi.actions.js
@@ -3,22 +3,23 @@ import { zubiService } from '../services/';
 
 export const zubiActions = {
   getMisConsultorias: getMisConsultorias,
-  getMisCapacitaciones: getMisCapacitaciones
+  getMisCapacitaciones: getMisCapacitaciones,
+  filterVisibleConsulting: filterVisibleConsulting
 };
 
 function getMisConsultorias() {
   return dispatch => {
       dispatch(request());
 
-      zubiService.misConsultorias()
+      zubiService.fetchConsultorias()
           .then(
-              consultorias => dispatch(success(consultorias)),
+              consulting => dispatch(success(consulting)),
               error => dispatch(failure(error.toString()))
           );
   };
 
   function request() { return { type: zubiConstants.ZUBI_GETALL_CONS_REQUEST } }
-  function success(consultorias) { return { type: zubiConstants.ZUBI_GETALL_CONS_SUCCESS, consultorias } }
+  function success(consulting) { return { type: zubiConstants.ZUBI_GETALL_CONS_SUCCESS, consulting, receivedAt: Date.now() } }
   function failure(error) { return { type: zubiConstants.ZUBI_GETALL_CONS_FAILURE, error } }
 }
 
@@ -36,4 +37,11 @@ function getMisCapacitaciones() {
   function request() { return { type: zubiConstants.ZUBI_GETALL_CAP_REQUEST } }
   function success(capacitaciones) { return { type: zubiConstants.ZUBI_GETALL_CAP_SUCCESS, capacitaciones } }
   function failure(error) { return { type: zubiConstants.ZUBI_GETALL_CAP_FAILURE, error } }
+}
+
+function filterVisibleConsulting (filter) {
+    return {
+      type: zubiConstants.ZUBI_SET_VISIBLE_CONS,
+      filter
+    }
 }
