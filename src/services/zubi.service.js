7b105fe6eed5359a910984226830977911e9c794
Lucas Natoli <lucas.natoli@gmail.com>
2019-05-08 17:53:16 -0300

    updated prod urls

--- src/services/zubi.service.js
+++ src/services/zubi.service.js
@@ -1,7 +1,7 @@
 import { authHeader } from '../helpers';
 
 const config = {
-  apiUrl : 'http://bigos.lan/wp-json/wp-react/v2'
+  apiUrl : 'http://zubi.com.ar/wp-json/wp-react/v2'
 }
 
 export const zubiService = {
