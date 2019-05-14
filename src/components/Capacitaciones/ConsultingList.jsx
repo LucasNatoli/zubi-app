a4ca5e65db2f3a048df87336b69d079a39250f4e
Lucas Natoli <lucas.natoli@gmail.com>
2019-05-14 16:23:44 -0300

    added capacitaciones list

---
+++ src/components/Capacitaciones/ConsultingList.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Avatar, Card, Icon, List } from 'antd'
+import { Link } from 'react-router-dom'
+
+const ConsultingList = ({ consulting, onConsultancyClick }) => (
+  <Card title="Capacitaciones">
+    <List
+      itemLayout="horizontal"
+      dataSource={consulting}
+      renderItem={cons => (
+        <Link href="/editar-consultoria" to="editar-consultoria">
+          <List.Item key={cons.id} onClick={() => onConsultancyClick(cons.id)} {...cons}>
+            <List.Item.Meta
+              avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+              title={cons.post_title}
+              description={cons.post_content}
+              extra={
+                <div class="image-placeholder">
+                  <h4>image placeholder text</h4>
+                </div>
+              }
+            />
+          </List.Item>
+        </Link>
+      )}
+    />
+  </Card>
+)
+/* 
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired
+  }).isRequired).isRequired,
+  onTodoClick: PropTypes.func.isRequired
+} */
+
+export default ConsultingList
