939bcef73a258d963e16c76082a04c8fcf75d63f
Lucas Natoli <lucas.natoli@gmail.com>
2019-06-19 18:21:46 -0300

    removed sample data

--- src/components/Consulting/Consulting.jsx
+++ src/components/Consulting/Consulting.jsx
@@ -3,35 +3,6 @@ import ConsultingList from './ConsultingList'
 import { connect } from 'react-redux'
 import { zubiActions } from '../../actions'
 
-/* 
-const data = [
-  {
-    id: 1, 
-    post_title: 'Consultoria 1',
-    post_content: "Ant Design, a design language for background applications, is refined by Ant UED Team",
-    post_status: "pending"
-  },
-  {
-    id: 2,
-    post_title: 'Consultoria 2',
-    post_content: "Ant Design, a design language for background applications, is refined by Ant UED Team",
-    post_status: "approved"
-  },
-  {
-    id: 3,
-    post_title: 'Consultoria 3',
-    post_content: "Ant Design, a design language for background applications, is refined by Ant UED Team",
-    post_status: "draft"
-  },
-  {
-    id: 4,
-    post_title: 'Consultoria 4',
-    post_content: "Ant Design, a design language for background applications, is refined by Ant UED Team",
-    post_status: "published"
-  },
-];
- */
-
 class Consulting extends React.Component {
   constructor(props) {
     super(props)
--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,72 +1,13 @@
 import React from 'react'
 import CoursesList from './CoursesList'
 
-const data = [
-  {
-    id: 1,
-    course_title: 'Capacitacion 1',
-    course_content: "Ant Design, a design language for background applications, is refined by Ant UED Team",
-    course_type: 'Capacitacion',
-    course_status: "pending",
-    course_progress: "100"
-  },
-  {
-    id: 2,
-    course_title: 'Capacitacion 1',
-    course_content: "Ant Design, a design language for background applications, is refined by Ant UED Team", 
-    course_type: 'Consultoria',
-    course_status: "approved",
-    course_progress: "80"
-  },
-  {
-    id: 3,
-    course_title: 'Consultoria 3',
-    course_content: "Ant Design, a design language for background applications, is refined by Ant UED Team", 
-    course_type: 'Consultoria',
-    course_status: "draft",
-    course_progress: "10"
-  },
-  {
-    id: 4,
-    course_title: 'Capacitacion 4',
-    course_content: "Ant Design, a design language for background applications, is refined by Ant UED Team", 
-    course_type: 'Consultoria',
-    course_status: "published",
-    course_progress: "70"
-  },
-  {
-    id: 5,
-    course_title: 'Capacitacion 5',
-    course_content: "Ant Design, a design language for background applications, is refined by Ant UED Team", 
-    course_type: 'Consultoria',
-    course_status: "published",
-    course_progress: "100"
-  },
-  {
-    id: 6,
-    course_title: 'Capacitacion 4',
-    course_content: "Ant Design, a design language for background applications, is refined by Ant UED Team", 
-    course_type: 'Consultoria',
-    course_status: "published",
-    course_progress: "0"
-  },
-  {
-    id: 7,
-    course_title: 'Capacitacion 4',
-    course_content: "Ant Design, a design language for background applications, is refined by Ant UED Team", 
-    course_type: 'Consultoria',
-    course_status: "published",
-    course_progress: "0"
-  }
-];
-
 class Courses extends React.Component {
   constructor(props) {
     super(props)
     this.onCourseClick = this.onCourseClick.bind(this)
   }
   onCourseClick(id) {
-    console.log(id)
+    console.log('course', id)
   }
   render() {
     const { courses } = this.props
