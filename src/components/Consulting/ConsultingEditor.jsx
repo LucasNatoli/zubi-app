023cc3fbcf109e5f85f4f76bfd8838f1d3c5fb58
Lucas Natoli <lucas.natoli@gmail.com>
2019-07-17 14:24:52 -0300

    added categorias hard

--- src/components/Consulting/ConsultingEditor.jsx
+++ src/components/Consulting/ConsultingEditor.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Alert, Avatar, Button, Card, Col, Form, Icon, Input, Layout, Row, Tabs, Upload } from 'antd';
+import { Alert, Avatar, Button, Select, Form, Icon, Input, Layout, Row, Tabs, Upload } from 'antd';
 import UploadJpg from '../UploadJpg'
-import './Consulting.css'
 import { connect } from 'react-redux'
 import { zubiActions } from '../../actions'
 
+const { Option } = Select
+const children = [
+  <Option key="1">Bancos</Option>,
+  <Option key="2">Emprendedor</Option>,
+  <Option key="3">Finanzas</Option>,
+  <Option key="4">Legales</Option>,
+  <Option key="5">Programa de Profesionalización</Option>,
+  <Option key="6">Proveedores/Compras</Option>,
+  <Option key="7">Selección de Personal</Option>  
+];
 
 class ConsultingForm extends React.Component {
   constructor(props) {
@@ -14,7 +23,6 @@ class ConsultingForm extends React.Component {
     this.props.post && console.log('rendering post', this.props.post)
     const { Header, Content } = Layout;
     const TabPane = Tabs.TabPane;
-
     return (
       <Layout className="inner-layout ">
         <Header className="header">
@@ -32,18 +40,36 @@ class ConsultingForm extends React.Component {
                 <Input
                   type="text"
                   name="titulo_de_la_consultoría"
-                  placeholder="Inserta el título de tu consultoría"
+                  placeholder="Utiliza un nombre especifico ej. Asesor en Contratos para Franquicias"
                   value={this.props.post && this.props.post.post_title}
                 />
                 <h4>Descripción</h4>
+                <p>Describe todo lo que puede aprender de tu experiencia, que tipo de consultoría virtual sobre ese tema podrías brindarle.</p>
                 <Input
                   type="text"
                   name="descripcion_de_la_consultoría"
-                  placeholder="Inserta la descripción de tu consultoría."
+                  placeholder="Maximo: 144 caracteres."
                   value={this.props.post && this.props.post.post_content}
                   maxLength="144" />
                 <h4>Categorías</h4>
-                <Input type="text" name="categoria_de_la_consultoría" placeholder="Inserta la descripción de tu consultoría." maxLength="144" />
+                {/* <Input type="text" name="categoria_de_la_consultoría" placeholder="Inserta la descripción de tu consultoría." maxLength="144" /> */}
+                <Select
+          mode="multiple"
+          size={'default'}
+          placeholder="Please select"
+          defaultValue={['a10', 'c12']}
+          //onChange={handleChange}
+          style={{ width: '100%' }}
+        >
+          {children}
+        </Select>
+                <h4>Precio</h4>
+                <Input
+                  type="text"
+                  name="precio_de_la_consultoría"
+                  placeholder="Indica el importe en pesos argentinos."
+                  //value={this.props.post && this.props.post.post_content}
+                  maxLength="5" />
               </Form>
               <h4>Perfiles de los instructores</h4>
               <Alert
@@ -59,14 +85,17 @@ class ConsultingForm extends React.Component {
             </TabPane>
             <TabPane key="2" tab="Imagen">
               <h4>Imagen de la consultoría</h4>
-              <p>Haz que tu consultoría destaque con una atractiva imagen de nuestro equipo de diseño basada en tus preferencias y estilo. Consigue tu imagen gratis.</p>
-              <p>Si creas la imagen por tu cuenta, tendrá que cumplir los estándares de calidad que deben cumplir las imágenes de los consultorías para que la aceptemos.</p>
-              <p>Directrices importantes: 750 x 422 píxeles, formato .jpg, .jpeg, .gif, o .png y sin texto en la imagen.</p>
+              <p>Elige una imagen que atraiga a tu público. Tendrá que cumplir los estándares de calidad que deben cumplir las imágenes para que la aceptemos.</p>
+              <p>Importante: 750 x 422 píxeles, formato .jpg, .jpeg, .gif, o .png y sin texto en la imagen.</p>
               <UploadJpg />
             </TabPane>
             <TabPane key="3" tab="Video">
-              <h4>Video Promocional</h4>
-              <p>Los estudiantes que ven un vídeo promocional bien hecho tienen 5 veces más probabilidades de matricularse en tu consultoría. Esa estadística se multiplica por 10 si los vídeos son excepcionalmente buenos. Aprende a hacer los tuyos impresionantes.</p>
+              <h4>Video de Presentacion</h4>
+              <h5>LLEGA A LOS DUEÑOS DE MICRO Y PEQUEÑAS EMPRESAS</h5>
+              <p>Si todavía no viste los consejos de como grabarte para tu video de presentación te invitamos a hacer <a href="https://www.zubi.com.ar/primeros-pasos-para-dar-de-alta-tu-perfil">click aquí.</a></p>
+              <p>Si ya viste los consejos y requisitos de como grabarte y tienes el equipo, ya estás en condiciones de enviarnos el video de Presentación para que nuestro equipo evalúe la calidad del mismo y te asesore.</p>
+              <p>Recuerda enviar la consultoria para revision y recibiras un mensaje a tu mail registrado dentro de las proximas 72 a 96 horas.</p>
+              <p>Recuerda revisar el correo no deseado si no recibiste respuesta.</p>
               <UploadJpg />
             </TabPane>
 
